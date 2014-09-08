b673c895763fb594bdc80c6175b4ae485b60c881
Willy Bruns <willy@entropi.info>
2014-09-08 01:19:56 -0700

    First commit for restless-api module :-)

---
+++ README.md
@@ -0,0 +1,98 @@
+# restless api: RESTful APIs for the highly impatient
+**Under development, check back soon for more documentation**
+
+restless-api takes a JSON spec of your APIs like you would send your user (no code inside it) and handles all the Express routing
+
+the goal has been as-simple-as-possible at all decision points, but there is support for model/controller breakdown and dependency injection if you want it
+
+## Example: see Node Annotator Store backend
+- The [NodeJS implementation of the Annotator backend](http://github.com/willy-b/node-annotator-store) is using restless-api
+
+## Quick start
+-  In your app file, call restless-api with JSON having two keys
+	* 'nouns': contains reference to a module (or optionally a controller and model) for each noun
+	* 'links': user-facing API spec (NO CODE, literally ready to send your user)
+
+- Example:
+```
+var restlessApi = require('../index.js');
+var apiRouter = restlessApi({
+    "nouns": {
+	"user": require("./lib/users.js")
+	/* 
+	   - Addl nouns:
+	     You can add more nouns here, e.g.:
+	        "products": require("./lib/products.js")
+
+	   - Model+controller and dependency injection:
+		You can also specify a model and controller separately, e.g.:
+		 "products": {
+		   	"model": require("./lib/productModel.js"),
+			"controller": require("./lib/productController.js")
+		 }
+	    -  The callbacks in the 'controller' module will be invoked with the model dependency injected
+	 */
+    },
+    "links": {
+	/* each key in 'links' is a REST noun from 'nouns' above */
+	"user": { 
+	    /* each key in a given noun denotes a REST endpoint (url+method)
+	       and will be routed to the callback in the noun module associated with the same key.
+
+	       for example, 'index' below will invoke the function 'index' in 'lib/users.js' --
+	       because that is the module associated with the noun in 'nouns' above 
+	    */
+	    "index": {
+		"url": "/users",
+		"method": "GET",
+		"desc": "Show a list of users"
+	    },
+	    "read": {
+		"url": "/users/:id",
+		"method": "GET",
+		"desc": "Get a user by ID"
+	    },
+	    "create": {
+		"url": "/users",
+		"method": "POST",
+		"desc": "Add a new user"
+	    },
+	    "update": {
+		"url": "/users/:id",
+		"method": "PUT/POST",
+		"desc": "Update an existing user with ID=:id"
+	    }
+	}
+    }
+});
+```
+
+- In your app file you use the router returned as normal:
+```
+// The routes above are relative; restless-api returns an express.Router object
+// which you can mount wherever you want
+// e.g. "/api":
+express.use("/api", apiRouter);
+
+express.listen(3000);
+console.log("Listening on port 3000!");
+```
+
+- And then create modules for each noun, e.g. `./lib/users.js` might be:
+```
+module.exports =  {
+	       index: function index(req, res, User) {
+	       //      ...
+	       },
+	       read: function read(req. res, User) {
+	       //       ...
+	       },
+//...	       	       	       
+};
+```
+- Where the 3rd "user" argument is the user "model" (this example used one unified model+controller for user noun, but if they were separate this would be the controller and it would get the model ref there)
+
+- more docs and example files coming soon...
+
+
+
---
+++ index.js
@@ -0,0 +1,134 @@
+(function() {
+    var express = require('express');
+    var _ = require('underscore');
+
+    module.exports = function(config) {
+	var MODULE_NAME = "restless-api";
+
+	if(!config || !config.nouns) {
+	    console.error("ERROR: " + MODULE_NAME + " module needs to be initialized with a 'config' object with a property, 'nouns', containing a map of noun-names to noun objects (a noun: 'aNoun': {'model': './models/aNounModel.js', 'controller': './controllers/aNounController.js'})");
+	    throw ("ERROR: could not load module " + MODULE_NAME + " since 'config' object needed for initialization was missing or didn't have the required 'nouns' property");
+	}
+
+	var nouns = getNounsFromJSON(config.nouns);
+	var router = generateRoutesFromJSON(config.links, nouns);
+
+	return router;
+    };
+
+
+    function generateRoutesFromJSON(spec, nouns) {
+	var router = new express.Router();
+	var nounsInSpec = _.keys(spec);
+
+	try {
+	    _.each(nounsInSpec, function(nounName) {
+		var noun = nouns[nounName];
+		if(!noun || typeof(noun) !== "object") {
+		    throw "ERROR: noun with name '" + nounName + "' missing from 'nouns' argument passed to generateRoutesFromJSON method";
+		}
+
+		var nounLinks = spec[nounName];
+		var linksInNoun = _.keys(nounLinks);
+		_.each(linksInNoun, function(linkName) {
+		    var link = nounLinks[linkName];
+		    if(!link || typeof(link) !== "object") {
+			throw "ERROR: link with name '" + linkName + "' missing from noun '" + nounName + "' passed in 'nouns' argument to generateRoutesFromJSON method";
+		    }
+		    
+		    addRouteForLink(router, linkName, link, noun);
+		});
+	    });
+	} catch(exc) {
+	    console.error("ERROR executing generateRoutesFromJSON:");
+	    console.error(exc);
+	    throw exc;
+	}
+	return router;
+    };
+
+    function addRouteForLink(router, linkName, link, noun) {
+	if(!_validateLink(link)) {
+	    throw "ERROR - addRouteForLink: Could not validate link '" + linkName + "'";
+	}
+
+	link.url = link.url.trim();
+
+	var methods = link.method.split(new RegExp(/\/\,\ /)).filter(function(method) {
+	    return (method && method.trim() !== "");
+	});
+	
+	var ALLOWED_METHODS = ["get", "post", "put", "delete"];
+	_.each(methods, function(method) {
+	    method = method.trim().toLowerCase();
+	    if(ALLOWED_METHODS.indexOf(method) === -1) {
+		throw "ERROR - addRouteForLink: don't understand method '" + method + "' for link '" + linkName + "'";
+	    }
+
+	    // For the really impatient we support just specifying a single file (controller+model) for a given noun :-)
+	    var controller = (noun.controller?noun.controller:noun);
+	    var model = (noun.model?noun.model:noun);
+
+	    var action = noun.controller[linkName];
+	    router[method](link.url, function(req, res) {
+		action(req, res, noun.model);
+	    });
+	});
+
+    };
+
+    function _validateLink(link) {
+	var valid = (link && link.url && link.method?true:false);
+	// TODO: validate the method (a string, array of strings, or a single string containing a list of methods separated by '/' or ',' or ' ')
+	return valid;
+    }
+
+    function requirePathValuesInJSON(json, config) {
+	var keys = _.keys(json);
+	var values = {};
+	_.each(keys, function(key) {
+	    if(config && key === "config") {
+		return;
+	    }
+	    var foundValue  = json[key];
+	    if(typeof(foundValue) === 'string') {
+		if(!config) {
+		    foundValue = require(foundValue);
+		} else if(config) {
+		    foundValue = require(foundValue)(config);
+		}
+	    }
+	    values[key] = foundValue;
+	});
+	return values;
+    }
+
+    function getNounsFromJSON(nounjson) {
+	var nounkeys = _.keys(nounjson);
+	var nouns = {};
+	_.each(nounkeys, function(nounName) {
+	    var nounconfig = null;
+	    var noun = nounjson[nounName];
+	    if(noun.config) { 
+		try {
+		    nounconfig = (typeof(noun.config) === "string"?require(noun.config):noun.config); 
+		} catch (exc) {
+		    console.error("ERROR: Couldn't load config for noun '" + nounName + "' from path '" + noun.config + "'");
+		    console.error(exc);
+		    throw exc;
+		}
+	    }
+	    try {
+		nouns[nounName] = requirePathValuesInJSON(noun, nounconfig);
+	    } catch(exc) {
+		console.error("ERROR: Couldn't load value for noun '" + nounName + "' from path '" + nounjson[nounName] + "':");
+		console.error(exc);
+		throw exc;
+	    }
+	});
+	return nouns;
+    }
+})();
+
+
+
